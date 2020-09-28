bd92e239e044964b66fdfca09cef5af0ca7b3f47
Mark Ryder <markmryder@gmail.com>
2020-09-28 18:26:51 -0400

    Added scripts for the game blocks to place properly on the map

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Node))]
+public class CubeEditor : MonoBehaviour
+{
+
+    
+    Node node;
+
+    private void Awake()
+    {
+        node = GetComponent<Node>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = node.GetGridSize();
+
+        transform.position = new Vector3
+        (
+            node.GetGridPos().x * gridSize,
+            0f, 
+            node.GetGridPos().y * gridSize
+        );
+    }
+
+    private void UpdateLabel()
+    {
+        Vector2Int gridPosition = node.GetGridPos();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = (gridPosition.x) + "," + (gridPosition.y);
+        textMesh.text = labelText;
+        gameObject.name = "Cube: " + labelText;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    const int gridSize = 10;
+    Vector2Int gridPosition;
+    public bool isExplored = false;
+    public bool isTraverable = true;
+    public Node exploredFrom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateColor();
+    }
+
+	private void UpdateColor()
+	{
+		if (isExplored)
+		{
+            SetTopColor(Color.blue);
+        }
+	}
+
+	public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int
+            (
+                Mathf.RoundToInt(transform.position.x / gridSize),
+                Mathf.RoundToInt(transform.position.z / gridSize)
+            );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        transform.Find("Top").GetComponent<MeshRenderer>().material.color = color;
+    }
+}
