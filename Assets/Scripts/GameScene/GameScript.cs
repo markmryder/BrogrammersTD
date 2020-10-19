6691c7818e1b0c1173460116b6425059c9bd985f
Mark Ryder <markmryder@gmail.com>
2020-10-18 21:04:25 -0400

    Created basic home screen and pause functionality of the game

---
+++ Assets/Scripts/GameScene/GameScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScript : MonoBehaviour
+{
+    [SerializeField] private GameObject menu;
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (!menu.activeInHierarchy)
+            {
+                PauseGame();
+            }
+            else if (menu.activeInHierarchy)
+            {
+                ContinueGame();
+            }
+        }
+    }
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+    }
+    private void ContinueGame()
+    {
+        Time.timeScale = 1;
+        menu.SetActive(false);
+    }
+
+    public void GotoHomeMenu()
+	{
+        SceneManager.LoadScene("HomeScene");
+	}
+}
